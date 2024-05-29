8d3ba6a3c9811cf967994875e983bfeacb6d56fe
Daniel Iván López García <lopez.garcia1319@gmail.com>
2024-05-29 08:10:37 -0600

    comenzando seccion 8

---
+++ BackendApp/Models/Beer.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace BackendApp.Models
+{
+    public class Beer
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int BeerId { get; set; }
+
+        public string Name {  get; set; }
+
+        public int BrandId { get; set; }
+
+        [ForeignKey("BrandId")]
+        public virtual Brand Brand { get; set; }
+    }
+}
---
+++ BackendApp/Models/Brand.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BackendApp.Models
+{
+    public class Brand
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int BrandId { get; set; }
+
+        public string Name { get; set; } 
+    }
+}
